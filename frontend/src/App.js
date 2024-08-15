c6d0d671faf8215571fc9faf1b5953cd06e615e5
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2024-08-15 12:40:46 +0300

    refactor: Remove unused role variable in App.js and AuthGuard.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/SignUp";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard/index";
